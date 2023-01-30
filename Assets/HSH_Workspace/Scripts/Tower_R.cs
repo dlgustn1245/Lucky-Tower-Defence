a5aed7b6dda9afda08c9655a47732bb8df17e5b9
kaylahee <kaylahee0410@gmail.com>
2023-01-30 20:38:59 +0900

    최신화

---
+++ Assets/HSH_Workspace/Scripts/Tower_R.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//타워 등급 열거형
+//public enum TowerGrade1 { Epic, Legendary, Unique, Rare, Uncommon, Common }
+
+[System.Serializable]
+public class Tower_R
+{
+    public string towerName;
+    public int towerAtk;
+    public GameObject towerPrefabs;
+    public TowerGrade1 towerGrade;
+    public int weight;
+
+    public Tower_R(Tower tower)//매개변수로 타워를 받는 생성자
+    {
+        //타워 목록 초기화
+        this.towerName = tower.towerName;
+        this.towerPrefabs = tower.towerPrefabs;
+        this.towerGrade = tower.towerGrade;
+        this.weight = tower.weight;
+    }
+}
