a02dccb5eeeafebb30ecdb4418fc4836fa5b008f
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2023-02-04 00:21:41 +0900

    Add Wave System

--- Assets/SS_WorkSpace/Scripts/EnemySpawner.cs
+++ Assets/SS_WorkSpace/Scripts/EnemySpawner.cs
@@ -4,82 +4,58 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    public EnemyGrade BossGrade = EnemyGrade.Boss; // Boss 등급 열거형 변수
-    public List<Enemy> Enemies = new List<Enemy>(); // Enemy클래스를 사용
-    [SerializeField]
-    private int maxBoss = 1; 
-    private int bossCount = 0;
-    [SerializeField]
-    private int maxEnemy = 0;
-    [SerializeField]
-    private int enemyCount = 0;
-    [SerializeField]
-    private int enemyIndex = 0;
-    [SerializeField]
-    private float spawnTime;        // 적 생성 주기
     [SerializeField]
     private Transform[] wayPoints;  // 스테이지 이동 경로
     [SerializeField]
     private GameObject enemyHPSliderPrefab;
     [SerializeField]
     private Transform canvasTransform;
+    
+    [SerializeField]
+    private Wave[] waves;
+    private Wave currentWave;
+    private int waveCount = 0;
+    private int currentWaveIndex = -1;
 
-    //맵에 존재하는 모든 적의 정보
-    private List<EnemyMove> enemyList;
-    private List<EnemyMove> EnemyList => enemyList;
 
     void Awake()
     {
-        // 적 리스트 메모리 할당
-        enemyList = new List<EnemyMove>();
-        StartCoroutine(SpawnEnemy());
     }
 
+    void Start()
+    {
+        StartWave();
+    }
+
+    public void StartWave()
+    {
+        StartCoroutine(SpawnEnemy());
+    }
     private IEnumerator SpawnEnemy()
     {
-        while (true)
+        while (waveCount < waves.Length)
         {
-            if (enemyCount == maxEnemy || bossCount == 1)
+            waveCount++;
+            currentWaveIndex++;
+            currentWave = waves[currentWaveIndex];
+            int spawnEnemyCount = 0;
+            while (spawnEnemyCount < currentWave.maxEnemyCount)
             {
-                if (enemyIndex == Enemies.Count || bossCount == maxBoss)
-                {
-                    StopCoroutine("SpawnEnemy");
-                }
-                enemyIndex += 1;
-                enemyCount = 0;
+                GameObject clone = Instantiate(currentWave.enemyPrefab);
+                EnemyMove enemyMove = clone.GetComponent<EnemyMove>();
+
+                enemyMove.Setup(wayPoints);
+               
+
+                SpawnEnemyHPSlider(clone);
+
+                spawnEnemyCount++;
+
+                yield return new WaitForSeconds(currentWave.spawnTime);
             }
-            // 보스일경우
-            if (Enemies[enemyIndex].enemyGrade == BossGrade)
-            {
-                //보스 소환
-                GameObject clonBoss = Instantiate(Enemies[enemyIndex].enemyPrefabs);
-                EnemyMove bossMove = clonBoss.GetComponent<EnemyMove>();
-                bossMove.Setup(this, wayPoints);
-                enemyList.Add(bossMove);
-                SpawnEnemyHPSlider(clonBoss);
-                bossCount += 1; //보스 카운트 1 증가
-            }
-            else
-            {
-                enemyCount++;
-                GameObject clon = Instantiate(Enemies[enemyIndex].enemyPrefabs);
-                EnemyMove enemyMove = clon.GetComponent<EnemyMove>();
-                enemyMove.Setup(this, wayPoints);
-                enemyList.Add(enemyMove);
-                SpawnEnemyHPSlider(clon);
-            }
-            
-            yield return new WaitForSeconds(spawnTime);
         }
     }
 
-    public void DestroyEnemy(EnemyMove enemyMove)
-    {
-        // 리스트에서 사망하는 적 정보 삭제
-        enemyList.Remove(enemyMove);
-        // 적 오브젝트 삭제
-        Destroy(enemyMove.gameObject);
-    }
 
     private void SpawnEnemyHPSlider(GameObject enemy)
     {
