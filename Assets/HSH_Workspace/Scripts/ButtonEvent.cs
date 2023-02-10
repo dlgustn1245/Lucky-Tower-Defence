52d9c2bbf4644ede7e3d6cad391a5abfd262caf6
kaylahee <kaylahee0410@gmail.com>
2023-02-10 17:33:30 +0900

    최신화

--- Assets/HSH_Workspace/Scripts/ButtonEvent.cs
+++ Assets/HSH_Workspace/Scripts/ButtonEvent.cs
@@ -4,11 +4,61 @@ using UnityEngine;
 
 public class ButtonEvent : MonoBehaviour
 {
-    public void UpgradeTower()
+	private bool state = false;
+
+	Tower currentTower;
+	ClickToShowData info;
+
+    public void UpgradeButton()
+    {
+        if (state == false)
+        {
+			gameObject.SetActive(true);
+			state = true;
+        }
+        else
+        {
+			gameObject.SetActive(false);
+			state = false;
+        }
+    }
+
+    public void UpgradeCommon()
 	{
-		
+		currentTower = info.target.GetComponent<TowerController>().tower;
+
+		if (currentTower.grade == TowerGrade.Common)
+        {
+			currentTower.attackSpeed++;
+			currentTower.damage++;
+			currentTower.range++;
+        }
 	}
 
+	public void UpgradeUnCommon()
+	{
+
+	}
+
+	public void UpgradeRare()
+	{
+
+	}
+
+	public void UpgradeUnique()
+	{
+
+	}
+
+	public void UpgradeEpic()
+	{
+
+	}
+
+	public void UpgradeLegendary()
+	{
+
+	}
 	public void SellTower()
 	{
 
